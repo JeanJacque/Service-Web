286bbd01420a7c735b1d12002582e264fa177835
webdev <webdev@webDevVM>
2015-05-15 15:11:49 +0200

    my fourth commit

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -8,46 +8,41 @@
  * Controller of the pooIhmExemplesApp
  */
 angular.module('pooIhmExemplesApp')
-  .controller('UsersCtrl', ['$scope', '$http', '$routeParams', 'filterFilter', function ($scope, $http, $routeParams, filterFilter) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('UsersCtrl', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+        $scope.update = function (user) {
+            $http.post('http://poo-ihm-2015-rest.herokuapp.com/api/Users/', user)
+                .success(function (data) {
+                    $location.path('/users/' + data.data.id);
+                });
+        };
 
-        $scope.master = {};
+        $scope.remove = function(id) {
 
-        $scope.update = function(user) {
-            $http.post('http://poo-ihm-2015-rest.herokuapp.com/api/Users/', $scope.user)
-            .success(function(data) {
+            $http.delete('http://poo-ihm-2015-rest.herokuapp.com/api/Users/' + id)
+                .success(function () {
 
-                $scope.go('/users/' + data.data.id);
+                    $location.path('/users');
 
+                });
+
+        };
+
+        $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Users')
+            .success(function (data) {
+                $scope.users = data.data;
             });
-        };
 
-        $scope.reset = function() {
-            $scope.user = angular.copy($scope.master);
-        };
-
-        $scope.greeting = '';
-
-        $scope.filteredArray = filterFilter($scope.users, greeting);
-
-
-        $scope.reset();
-
-    $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Users')
-      .success(function(data) {
-        $scope.users = data.data;
-      });
-
-    if($routeParams.userId) {
-      $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Users/' + $routeParams.userId)
-      .success(function(data) {
-        if (data.status == "success") {
-          $scope.currentUser = data.data;
+        if ($routeParams.userId) {
+            $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Users/' + $routeParams.userId)
+                .success(function (data) {
+                    if (data.status == "success") {
+                        $scope.currentUser = data.data;
+                    }
+                });
         }
-      });
-    }
-  }]);
+    }]);
